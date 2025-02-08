68595c091d5702c9cb9d27f995ea0957043fe2cf
KiranB7302 <yuvakiranbadarla17@gmail.com>
2025-02-08 17:25:26 +0530

    updated code

---
+++ projectJenkins/src/test/java/tests/TestExample6.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class TestExample6 {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
